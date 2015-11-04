1d081ed6fa6d515e3a618dc5dfa29a62662619cd
James Foster <jamesfoster85@gmail.com>
2015-11-04 13:31:57 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Catster::Application.config.secret_key_base = '78f4a2e5a7f82ea91237b7fbea5dd15866576eb5e94cf32609b84cb2bbd20b5834933d839361a2fcc4dbb5c50c90fa94ea69bc891119fc5fdf3bfe14c2b8f3ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Catster::Application.config.session_store :cookie_store, key: '_catster_session'
